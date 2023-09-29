d77c094e7e34b436f1307a2ed441c3f5453fe2c0
Auccelio <auxerreauccelio@gmail.com>
2023-09-29 19:02:49 -0300

    Read inputs from users
    Signed-off-by: Auccelio <auxerreauccelio@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ bmi - body mass index from terminal
 # Number 1
 I created the Class Person in a new file just to make this code clean and better for refactoring.
 
+# Number 2
+I received someone's name, weight and height from terminal, handling some exceptions as null value for String and placed zero in double variables when it is necessary.
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main(){
+  String namee;
+  double weightt;
+  double heightt;
+
+  stdout.write("Enter your name please!");
+  namee = stdin.readLineSync() ?? " ";
+  //print(namee);
+
+  stdout.write("Enter your weight in kilograms please!");
+  weightt = double.parse(stdin.readLineSync() ?? "0.00 ");
+  //print(weightt);
+
+  stdout.write("Enter your height in meters please!");
+  heightt = double.parse(stdin.readLineSync() ?? "0.00");
+  //print(heightt);
 
 }
